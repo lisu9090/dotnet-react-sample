89c3a0776e4295630deb9390137eecb60f885f76
Tomasz Lis <t.lis@enbw.com>
2023-09-10 21:15:41 +0200

    Improved responsivity of lansking page; created log-in and account pages

--- src/AwesomeApp.UI/src/pages/index.tsx
+++ src/AwesomeApp.UI/src/pages/index.tsx
@@ -5,24 +5,30 @@ import Link from "next/link"
 export default function RootPage() {
   return (
     <Grid 
-      container 
+      container
       className="page-container"
       alignItems="center"
       justifyContent="center"
     >
-      <Grid item xs={6}>
+      <Grid
+        item 
+        xs={12} 
+        sm={10} 
+        md={6}
+        className="main-box"
+      >
         <Paper>
           <Grid 
             container 
             spacing={6} 
             padding={2}
+            direction="column"
             alignItems="center"
             justifyContent="center"
           >
             <Grid 
               item 
               container 
-              xs={12} 
               justifyContent="center"
             >
               <Typography variant="h5">Welcome to AwesomeApp</Typography>
@@ -30,41 +36,57 @@ export default function RootPage() {
             <Grid 
               item 
               container 
-              xs={12} 
               direction="column"
               alignItems="center"
               justifyContent="center"
             >
-              <Typography variant="caption">Powered by</Typography>
-              <Image src="next.svg" width={500} height={500} alt="Next logo"/>
+              <Typography variant="caption" className="mb-2">Powered by</Typography>
+              <Image 
+                src="next.svg" 
+                width={500} 
+                height={500} 
+                alt="Next logo"
+              />
             </Grid>
             <Grid 
               item 
-              container 
-              xs={4} 
+              container
               justifyContent="center"
             >
-              <Link href="/create-account">
-                <Button variant="outlined">Log In</Button>
-              </Link>
-            </Grid>
-            <Grid 
-              item 
-              container 
-              xs={4} 
-              justifyContent="center"
-            >
-              <span>or</span>
-            </Grid>
-            <Grid 
-              item 
-              container 
-              xs={4} 
-              justifyContent="center"
-            >
-              <Link href="/create-account">
-                <Button variant="outlined">Create Account</Button>
-              </Link>
+              <Grid 
+                item 
+                container 
+                xs={4} 
+                direction="column"
+                alignItems="stretch" 
+              >
+                <Link href="/log-in">
+                  <Button className="w-full" variant="outlined">Log In</Button>
+                </Link>
+              </Grid>
+              <Grid 
+                item
+                container
+                xs={1}
+                alignItems="center"
+                justifyContent="center"
+              >
+                <Typography variant="caption">or</Typography>
+              </Grid>
+              <Grid 
+                item 
+                container 
+                xs={4} 
+                direction="column"
+                alignItems="stretch" 
+              >
+                <Link href="/create-account">
+                  <Button className="w-full" variant="outlined">Create Account</Button>
+                </Link>
+              </Grid>
+
+
+
             </Grid>
           </Grid>
         </Paper>
