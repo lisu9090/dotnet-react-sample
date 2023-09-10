5c8baf76959c1b9e6ea1847a74f00d511db7cfe5
Tomasz Lis <t.lis@enbw.com>
2023-09-10 15:52:12 +0200

    Mui added

--- src/AwesomeApp.UI/src/pages/index.tsx
+++ src/AwesomeApp.UI/src/pages/index.tsx
@@ -1,10 +1,25 @@
+import { Grid, Paper } from "@mui/material"
+import Image from "next/image"
 import Link from "next/link"
 
 export default function RootPage() {
   return (
     <>
-      <div>This is root page</div>
-      <Link href="/create-account">Create Account</Link>
+      <Paper className="main-container">
+        <div>Welcome to AwesomeApp</div>
+        <Image src="next.svg" width={500} alt="Next logo"/>
+        <Grid container spacing={2}>
+          <Grid>
+            <Link href="/create-account">Log In</Link>
+          </Grid>
+          <Grid>
+            <span>or</span>
+          </Grid>
+          <Grid>
+            <Link href="/create-account">Create Account</Link>
+          </Grid>
+        </Grid>
+      </Paper>
     </>
   )
 }
