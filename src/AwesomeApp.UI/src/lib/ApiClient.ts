5984168ca9d7795463b756f9453cb5c2001ed7e8
Tomasz Lis <t.lis@enbw.com>
2023-11-12 23:54:00 +0100

    removed trash files

--- src/AwesomeApp.UI/src/lib/ApiClient.ts
+++
@@ -1,5 +0,0 @@
-export async function fetchData(): Promise<any> {
-  const response = await fetch('https://localhost:7075/WeatherForecast')
-
-  return []
-}
--- src/AwesomeApp.UI/src/types/models/CustomerType.ts
+++
@@ -1,4 +0,0 @@
-export enum CustomerType {
-  private = 'private',
-  company = 'company'
-}
