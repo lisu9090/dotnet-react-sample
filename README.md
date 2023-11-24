cf1603155f32002692b6f3fc70d9ebb692e1eb70
Tomasz Lis <lisu9090@gmail.com>
2023-11-24 10:04:19 +0100

    Project cleanup and description (#10)
    * removed trash files
    
    * improved SimpleFOrmValidation
    
    * readme
    
    * Running section
    
    * roadmap
    
    * design concepts wip
    
    * readme completed
    
    * fixed typos
    
    ---------
    
    Co-authored-by: Tomasz Lis <t.lis@enbw.com>

--- README.md
+++ README.md
@@ -1,1 +1,85 @@
-# dotnet-react-sample
+# Welcome to Awesome Web App
+
+Powered by the most common web technologies and frameworks project which gives you initial setup and much more...
+
+## Technology stack
+
+- ASP.NET Core Web API
+  - .NET 7
+  - MediatR
+  - MSTest
+- Next.js
+  - React 18
+  - TypeScript
+  - MUI (Material UI)
+  - Tailwind CSS
+  - iron-session
+
+## Design concepts
+
+Concept behind the project is to speed up development process by delivering extensible, ready-to-develop web app. The app takes advantage of common technology stack, architecture, best practices and design patterns that are widely used in modern web systems. Moreover it ships with bunch of mechanisms, which usually need to be developed from scratch on project startup. This includes user authentication, role-based authorization, error handling, logging and localization. All of these can be found in Awesome Web App.
+
+### Architecture
+
+System consists of three main components: 
+- **RESTful web API** - REST compliant web API, which serves business logic and can be easily consumed by multiple services.
+- **App backend** - acts as a proxy to API, handles user authentication, authorization and HTTP errors. It can be extended to consume multiple web services. 
+- **App frontend** - application user interface.
+
+### RESTful web API
+
+Created using ASP.NET Core Web API is a REST compliant web service. It takes advanteage of Layered Architecture and implements Command-Query Responsibility Segregation (CQRS) and Repository patterns. Each layer is separated with contracts that hide implementations and enables power of Dependency Injection. Code quality is ensured by set of Unit Tests.
+
+### App backend
+
+Next.js projects ship with build-in API. Here, API acts as application backend that fetches data from other services, handles user authentication, role-based authorization and HTTP Errors.   
+
+### App frontend
+
+App frontend is React application that takes advantage of Next.js framework. It uses Static Site Generation for pages that do not require user context and Server-Side Rendering in order to manage user access.
+
+## Running
+
+Application has no production environment yet, therefore you will need to compile source files.
+
+### Prerequisites
+
+In order to run app you will need following software to be installed:
+- Visual Studio IDE or .NET CLI
+- Nodejs
+
+### Run in debug mode (Windows)
+
+1. API
+    1. Open src\AwesomeApp.API\AwesomeApp.API.sln file with Visual Studio
+    2. From menu in the top of window select Debug > Start debugging or press F5 button
+2. App
+    1. Create config file .env.local in src/AwesomeApp.UI folder based on example.env. Provide value for SESSION_PASSWORD variable (this needs to be strong password of at least 32 characters; you can generate one [here](https://1password.com/password-generator/))
+    2. Open src\AwesomeApp.UI folder in termianl
+    3. Run commands 
+    ``` bash
+    npm install
+    npm run dev
+    ```
+    4. Open browser and access http://localhost:3000
+
+## Roadmap
+
+- [x] Create API and App projects
+- [x] Setup projects structure
+- [x] Setup communication between API and App Backend
+- [x] Setup communication between App Backend and App frontend
+- [ ] Setup error handling in API
+- [ ] Setup error handling mechanism in both App Backend and frontend
+- [ ] (Optional) Setup sample logging mechanism in API and App Backend
+- [ ] Secure communication between API and App Backend (simple subscription key)
+- [ ] Secure communication between App backend and App frontend (user authentication and authorization)
+- [ ] Secure App routing and enable role-based user authorization
+- [ ] Enable App localization (string translations)
+- [x] Feature: Landing, Error and NotFound pages
+- [x] Feature: Create user account
+- [x] Feature: Authenticate user
+- [ ] Feature: UserDetails page that allows to view and edit account data (secured)
+- [ ] Feature: AdminPanel page that allows admins to manage accounts (secured)
+- [ ] Unit tests
+- [ ] Docker support
--- src/AwesomeApp.UI/src/frontend/libs/SimpleFormValidation.ts
+++ src/AwesomeApp.UI/src/frontend/libs/SimpleFormValidation.ts
@@ -3,7 +3,7 @@ import { useMemo, useState } from "react";
 const emailRegExp = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
 const strongPasswordRegExp = /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{8,20})/
 
-export type ValidatorFn = (value: string) => string
+export type ValidatorFn = (fieldValue: string, formValue: any) => string
 export type ValidateFormFieldFn = (fieldName: string, fieldValue: string) => void
 
 export interface SimpleFormValidation {
@@ -19,36 +19,46 @@ export interface FormValidators {
 
 export const requiredValidator: (message?: string) => ValidatorFn = 
   (message: string = "Value is required") => 
-    (value: string) => value?.trim().length > 0 ? '' : message
+    (fieldValue: string) => fieldValue?.trim().length > 0 ? '' : message
 
 export const minLengthValidator: (message?: string, minLength?: number) => ValidatorFn = 
   (message: string = "Value must have more then 3 characters", minLength: number = 3) => 
-    (value: string) => value?.trim().length >= minLength ? '' : message
+    (fieldValue: string) => fieldValue?.trim().length >= minLength ? '' : message
 
 export const positiveValueValidator: (message?: string) => ValidatorFn = 
   (message: string = "Value must be positive number") => 
-    (value: string) => Number.parseFloat(value) >= 0 ? '' : message
+    (fieldValue: string) => Number.parseFloat(fieldValue) >= 0 ? '' : message
 
 export const emailValidator: (message?: string) => ValidatorFn = 
   (message: string = "Value must be valid email") => 
-    (value: string) => emailRegExp.test(value) ? '' : message
+    (fieldValue: string) => emailRegExp.test(fieldValue) ? '' : message
 
 export const strongPasswordValidator: (message?: string) => ValidatorFn = 
   (message: string = "Value must contain capital letter, small letter, number, special character and be between 8 and 20 characters long") => 
-    (value: string) => strongPasswordRegExp.test(value) ? '' : message
+    (fieldValue: string) => strongPasswordRegExp.test(fieldValue) ? '' : message
 
-export function useSimpleFormValidation(initialValue: SimpleFormValidation, validators: FormValidators): {
+export const fieldEqualityValidator: (otherFieldName: string, message?: string) => ValidatorFn = 
+  (otherFieldName: string, message: string = "Value must be equal to value in field " + otherFieldName) => 
+    (fieldValue: string, formValue: any) => fieldValue === formValue[otherFieldName] ? '' : message
+
+export function useSimpleFormValidation<T> (
+  initialFormValue: T, 
+  initialFormValidationValue: SimpleFormValidation, 
+  validators: FormValidators): {
+  formValue: T;
   formValidation: SimpleFormValidation;
+  setFormValue: (value: T) => void;
   validateFormField: ValidateFormFieldFn;
   setFormValidators: (value: FormValidators) => void;
 } {
-  const [formValidation, setFormValidation] = useState<SimpleFormValidation>(initialValue)
+  const [formValue, setFormValue] = useState<T>(initialFormValue)
+  const [formValidation, setFormValidation] = useState<SimpleFormValidation>(initialFormValidationValue)
   const [formValidators, setFormValidators] = useState<FormValidators>(validators)
 
   const validateFormField = useMemo<ValidateFormFieldFn>(
     () => (fieldName: string, fieldValue: string) => {
       const validationError = formValidators[fieldName]
-        ?.map(validator => validator(fieldValue))
+        ?.map(validator => validator(fieldValue, formValue))
         .filter(errorMessage => !!errorMessage)
         [0] ?? ''
 
@@ -62,16 +72,18 @@ export function useSimpleFormValidation(initialValue: SimpleFormValidation, vali
         fieldErrors: fieldErrors
       })
     },
-    [formValidation, formValidators, setFormValidation]
+    [formValue, formValidation, formValidators, setFormValidation]
   )
 
   return useMemo(
     () => ({
+      formValue,
       formValidation,
+      setFormValue,
       validateFormField,
       setFormValidators
     }),
-    [formValidation, validateFormField, setFormValidators]
+    [formValue, formValidation, setFormValue, validateFormField, setFormValidators]
   )
 }
 
