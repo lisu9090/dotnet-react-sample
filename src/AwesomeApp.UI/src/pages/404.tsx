c7b287aa9891b72c24b66d018c6b2f0fd1d0acfb
Tomasz Lis <t.lis@enbw.com>
2024-05-29 11:15:59 +0200

    improved 404 and 403 pages

--- src/AwesomeApp.UI/src/pages/404.tsx
+++ src/AwesomeApp.UI/src/pages/404.tsx
@@ -1,11 +1,29 @@
+import { PAGE_HOME } from '@/common/consts'
 import { PageBox } from '@/frontend/components'
-import { Typography } from '@mui/material'
+import { Button, Grid, Typography } from '@mui/material'
+import Link from 'next/link'
 import { ReactElement } from 'react'
 
 export default function NotFoundPage(): ReactElement {
   return (
     <PageBox>
-      <Typography variant="h5">404 - Not found</Typography>
+      <Grid container justifyContent="space-between">
+        <Grid item xs={4}>
+          <Typography variant="h5">
+            404 - Not found
+          </Typography>
+        </Grid>
+        <Grid item xs={4}>
+          <Link href={PAGE_HOME}>
+            <Button
+              className="w-full"
+              variant="outlined"
+            >
+              Return to home
+            </Button>
+          </Link>
+        </Grid>
+      </Grid>
     </PageBox>
   )
 }
