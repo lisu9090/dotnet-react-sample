858cb19a0aaefe14be637ff2e52d8e5eabfe40f2
Tomasz Lis <t.lis@enbw.com>
2024-10-07 20:38:04 +0200

    added cert generation windows scripts

--- src/Nginx/cert-gen.bat
+++ src/Nginx/cert-gen.bat
@@ -2,6 +2,6 @@ if not exist ".cert\" mkdir .cert
 
 cd .cert
 
-openssl genrsa -out client.key 2048 && ^
-openssl req -new -key client.key -out client.csr && ^
+openssl genrsa -out client.key 2048
+openssl req -new -key client.key -out client.csr
 openssl x509 -req -in client.csr -signkey client.key -out client.crt
