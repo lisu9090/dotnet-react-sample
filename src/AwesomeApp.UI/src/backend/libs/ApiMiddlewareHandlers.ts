0af531ce02786d56f9affd9422304d5d5c1d578b
Tomasz Lis <t.lis@enbw.com>
2024-10-08 11:06:07 +0200

    PR fixes

--- src/AwesomeApp.UI/src/backend/libs/ApiMiddlewareHandlers.ts
+++ src/AwesomeApp.UI/src/backend/libs/ApiMiddlewareHandlers.ts
@@ -132,7 +132,7 @@ export function withErrorHandling(handler: NextApiHandler): NextApiHandler {
       await handler(req, res)
     }
     catch (e) {
-      console.log(e)
+      console.error(e)
 
       res.status(HttpStatusCode.InternalServerError)
         .send('Internal server error - ' + e)
