75f319c53cf0490fa8a38850d01599c44661dc50
Tomasz Lis <t.lis@enbw.com>
2024-10-29 16:49:56 +0100

    experimenting with scanner

--- src/AwesomeApp.UI/gulpfile.js
+++ src/AwesomeApp.UI/gulpfile.js
@@ -1,17 +1,22 @@
 var gulp = require('gulp')
 var scanner = require('i18next-scanner')
+var ts = require("gulp-typescript")
+var tsProject = ts.createProject("tsconfig.json")
 
-gulp.task('i18next', function () {
-  return gulp.src(['src/**/*.{ts,tsx}'])
-    .pipe(scanner({
-      lngs: ['en', 'de', 'pl'],
-      resource: {
-        // the source path is relative to current working directory
-        loadPath: 'public/locales/{{lng}}.json',
-
-        // the destination path is relative to your `gulp.dest()` path
-        savePath: 'locales/{{lng}}.json'
-      }
-    }))
-    .pipe(gulp.dest('public'))
-})
+gulp.task(
+  'i18next',
+  function () {
+    return tsProject
+      .src()
+      .pipe(tsProject())
+      .js
+      .pipe(scanner({
+        lngs: ['en', 'de', 'pl'],
+        resource: {
+          loadPath: 'public/locales/{{lng}}.json',
+          savePath: 'public/locales/{{lng}}.json'
+        }
+      }))
+      .pipe(gulp.dest('.'))
+  }
+)
