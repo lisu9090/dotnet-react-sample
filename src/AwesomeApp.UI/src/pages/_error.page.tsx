aedf5268774056f4f196669d8c579e9002da04d5
Tomasz Lis <t.lis@enbw.com>
2024-11-05 12:54:05 +0100

    added error page translations

--- src/AwesomeApp.UI/src/pages/_error.page.tsx
+++ src/AwesomeApp.UI/src/pages/_error.page.tsx
@@ -5,6 +5,7 @@ import { HttpStatusCode } from 'axios'
 import { NextPageContext } from 'next'
 import Link from 'next/link'
 import { ReactElement } from 'react'
+import { Trans } from 'react-i18next'
 
 type Props = {
   statusCode: number;
@@ -16,14 +17,14 @@ type Props = {
  * @returns Page Component 
  */
 function ErrorPage({ statusCode }: Readonly<Props>): ReactElement {
-  const getErrorMessage = () => {
+  const getErrorMessageTranslation = () => {
     switch (statusCode) {
       case HttpStatusCode.NotFound:
-        return 'Not found (404)'
-      case HttpStatusCode.Forbidden:
-        return 'Forbidden (403)'
-      default:
-        return 'Something went wrong' + statusCode ? ` (${statusCode})` : ''
+        return <Trans>Not found (404)</Trans>
+      case HttpStatusCode.Forbidden: 
+        return <Trans>Forbidden (403)</Trans>
+      default: 
+        return <Trans>Something went wrong ({{statusCode}})</Trans>
     }
   }
 
@@ -38,9 +39,7 @@ function ErrorPage({ statusCode }: Readonly<Props>): ReactElement {
         <PageBox>
           <Grid container justifyContent="space-between">
             <Grid item xs={4}>
-              <Typography variant="h5">
-                {getErrorMessage()}
-              </Typography>
+              <Typography variant="h5">{getErrorMessageTranslation()}</Typography>
             </Grid>
             <Grid item xs={4}>
               <Link href={PAGE_HOME}>
@@ -48,7 +47,7 @@ function ErrorPage({ statusCode }: Readonly<Props>): ReactElement {
                   className="w-full"
                   variant="outlined"
                 >
-                  Return to home
+                  <Trans>Return to home</Trans>
                 </Button>
               </Link>
             </Grid>
